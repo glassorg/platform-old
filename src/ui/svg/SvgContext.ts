c8c82d80eb91b86b9bda953bcd88ca96cb4c169f
Kris Nye <krisnye@gmail.com>
2019-07-09 11:52:40 -0700

    removing obsolete factories

--- src/ui/svg/SvgContext.ts
+++
@@ -1,7 +0,0 @@
-import svg, { SvgFactories } from "./";
-import Context from "../Context";
-import TypedContext from "../TypedContext";
-
-export default function(c: Context) {
-    return TypedContext.create<SvgFactories>(c, svg)
-}
