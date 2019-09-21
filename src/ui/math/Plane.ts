3a13ac1328a2962e387462bd38839f291210f665
Kris Nye <krisnye@gmail.com>
2019-09-20 21:58:37 -0700

    made plane impl boundingshape

--- src/ui/math/Plane.ts
+++ src/ui/math/Plane.ts
@@ -1,10 +1,13 @@
 import Vector3 from "./Vector3"
+import BoundingShape from "./BoundingShape"
+import Capsule from "./Capsule"
+import Line from "./Line"
 
 /**
  * A Plane is represented by a normal vector
  * and a distance from the origin to the closest point on the plane.
  */
-export default class Plane {
+export default class Plane implements BoundingShape {
 
     normal: Vector3
     distance: number
@@ -20,6 +23,14 @@ export default class Plane {
             this.normal.y * this.distance,
             this.normal.z * this.distance,
         )
+    }
+
+    intersects(capsule: Capsule): boolean {
+        throw new Error("kpi")
+    }
+
+    getClosestPoint(line: Line): Vector3 {
+        throw new Error("kpi")
     }
 
 }
