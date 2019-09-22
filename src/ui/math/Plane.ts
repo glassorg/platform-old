1e7a09eff5184f20c531a93972013337c75e6d40
KodyJKing <kody.j.king@gmail.com>
2019-09-22 16:58:05 -0700

    Implemented intersects and getClosestPoint on Plane.

--- src/ui/math/Plane.ts
+++ src/ui/math/Plane.ts
@@ -2,6 +2,8 @@ import Vector3 from "./Vector3"
 import BoundingShape from "./BoundingShape"
 import Capsule from "./Capsule"
 import Line from "./Line"
+import { equivalent } from "."
+import ISupported from "./ISupported"
 
 /**
  * A Plane is represented by a normal vector
@@ -25,12 +27,31 @@ export default class Plane implements BoundingShape {
         )
     }
 
-    intersects(capsule: Capsule): boolean {
-        throw new Error("kpi")
+    intersects(shape: ISupported): boolean {
+        let point = this.getClosestPointToOrigin()
+        let max = shape.support(this.normal)
+        let min = shape.support(this.normal.negate())
+        let pMax = max.subtract(point)
+        let pMin = min.subtract(point)
+        let maxDist = this.normal.dot(pMax)
+        let minDist = this.normal.dot(pMin)
+        return Math.sign(maxDist) != Math.sign(minDist)
     }
 
     getClosestPoint(line: Line): Vector3 {
-        throw new Error("kpi")
+        let point = this.getClosestPointToOrigin()
+        let ap = point.subtract(line.a)
+        let distance = ap.dot(this.normal)
+        if (equivalent(distance, 0))
+            return line.a
+
+        let heading = line.b.subtract(line.a)
+        let speed = heading.dot(this.normal)
+        if (equivalent(speed, 0))
+            return line.a.subtract(this.normal.projection(line.a))
+
+        let dt = distance / speed
+        return line.getPosition(dt)
     }
 
 }
