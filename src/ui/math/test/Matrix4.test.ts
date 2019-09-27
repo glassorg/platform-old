492b5941696b8f5e95ce9d5f5d84d04b73238886
KodyJKing <kody.j.king@gmail.com>
2019-09-26 19:00:51 -0700

    Added test to verify that Matrix4 multiplication isn't backwards.

--- src/ui/math/test/Matrix4.test.ts
+++ src/ui/math/test/Matrix4.test.ts
@@ -15,3 +15,25 @@ test("Matrix4", assert => {
     assert.false(a.equivalent(b))
     assert.true(a.equivalent(c))
 })
+
+test("Matrix4 / Vector Associativity", assert => {
+    let x = new Vector3(1, 0, 0)
+    let y = new Vector3(0, 1, 0)
+    let z = new Vector3(0, 0, 1)
+    let xPrime = x.transform(
+        Matrix4.rotation(y, Math.PI / 2)
+    ).transform(
+        Matrix4.rotation(z, Math.PI / 2)
+    ) // = Rz (Ry x)
+    let xPrime2 = x.transform(
+        Matrix4.rotation(z, Math.PI / 2)
+            .multiply(Matrix4.rotation(y, Math.PI / 2))
+    ) // = (Rz Ry) x
+    let xPrime3 = x.transform(
+        Matrix4.rotation(y, Math.PI / 2)
+            .multiply(Matrix4.rotation(z, Math.PI / 2))
+    ) // = (Ry Rz) x
+
+    assert.true(xPrime.equivalent(xPrime2))
+    assert.false(xPrime3.equivalent(xPrime2))
+})
