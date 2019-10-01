9f7e96f19bb82c08378a7a45d8e6902eb2ecddd6
Kris Nye <krisnye@gmail.com>
2019-09-30 22:06:57 -0700

    removed dead code

--- src/ui/graphics/DataStream.ts
+++ src/ui/graphics/DataStream.ts
@@ -1,14 +1,4 @@
-import * as GL from "./GL"
-import IndexBuffer from "./IndexBuffer"
-import VertexStream from "./VertexStream"
 import VertexFormat from "./VertexFormat"
-
-const sizeOfUint16 = 2
-
-const pointOffsets = [0]
-const lineOffsets = [0, 1]
-const triangleOffsets = [0, 1, 2]
-const quadOffsets = [0, 1, 2, 2, 1, 3]
 
 export default abstract class DataStream {
 
