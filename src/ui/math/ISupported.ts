ed5ea4b89cd4a54102fc83f4cf331b07edb17b9a
KodyJKing <kody.j.king@gmail.com>
2019-12-24 20:59:42 -0800

    Added AABB with raytrace method. Moved sphere raytrace out of ray into Sphere class.

--- src/ui/math/ISupported.ts
+++ src/ui/math/ISupported.ts
@@ -1,5 +1,4 @@
 import Vector3 from "./Vector3"
-
 export default interface ISupported {
     // Returns the furthest point along a direction.
     support(v: Vector3): Vector3
--- src/ui/math/test/Ray.test.ts
+++ src/ui/math/test/Ray.test.ts
@@ -3,17 +3,34 @@ import Ray from "../Ray"
 import Vector3 from "../Vector3"
 import Sphere from "../Sphere"
 import { equivalent } from ".."
+import AABB from "../AABB"
 
-test( "Ray", assert => {
-    let r = new Ray( new Vector3( 0, Math.sin( Math.PI / 4 ), 0 ), new Vector3( 1, 0, 0 ) )
-    let s = new Sphere( new Vector3( 1, 0, 0 ), 1 )
-    let d = r.distanceToSphere( s )
-    if ( d === null )
+test("Cast on Sphere", assert => {
+    let r = new Ray(new Vector3(0, Math.sin(Math.PI / 4), 0), new Vector3(1, 0, 0))
+    let s = new Sphere(new Vector3(1, 0, 0), 1)
+    let d = s.raycastDistance(r)
+    if (d === null)
         return assert.fail()
     assert.true(
         equivalent(
-            ( 1 - d ),
-            Math.sqrt( 2 ) / 2
+            (1 - d),
+            Math.sqrt(2) / 2
         )
     )
-} )
+})
+
+test("Cast on AABB", assert => {
+    let r = new Ray(new Vector3(0, 0, 0), new Vector3(0, 1, 0))
+    let box = new AABB(new Vector3(0, 10, 0), new Vector3(1, 1, 1))
+    let hit = box.raycast(r)
+    if (hit === null)
+        return assert.fail()
+    let normal = box.normal(hit)
+    assert.deepEqual(
+        { hit, normal },
+        {
+            hit: new Vector3(0, 9, 0),
+            normal: new Vector3(0, -1, 0)
+        }
+    )
+})
