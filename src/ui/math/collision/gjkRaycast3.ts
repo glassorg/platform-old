d433d713dceecc6dc4525dee77695d5f76a875b7
KodyJKing <kody.j.king@gmail.com>
2020-01-10 02:12:17 -0800

    Added a bit of documentation to gjkRaycast.

--- src/ui/math/collision/gjkRaycast3.ts
+++ src/ui/math/collision/gjkRaycast3.ts
@@ -4,9 +4,11 @@ import { SupportFunction, nonMultiple, checkEdge, normalForEdge, checkFace, chec
 const ex = new Vector3(1, 0, 0)
 const ey = new Vector3(0, 1, 0)
 
-export function gjkRaycastInitialTriangle(support: SupportFunction, heading: Vector3, debug?: any) {
+// This method projects a 3D shape onto a plane normal to heading passing through the origin, performs 2D GJK on this shape.
+// If this finds a triangle containing the ray origin, than the ray passes through the 3D shape.
+export function silhouetteGjk(support: SupportFunction, heading: Vector3, debug?: any) {
     function project(vector: Vector3) { return heading.rejection(vector) }
-    const maxIterations = 100
+    const maxIterations = 10
     let initialHeading = project(heading.cross(ex).equivalent(Vector3.zero) ? ey : ex)
     let initialPoint = support(initialHeading)
     let searchDirection = project(initialPoint.negate())
@@ -77,11 +79,19 @@ export function gjkRaycastInitialTriangle(support: SupportFunction, heading: Vec
     }
 }
 
+// gjkRaycast3 returns the time and normal at which the input ray hits the input geometry, given as a support function.
+// It starts by finding an initial triangle contained by the input geometry which the ray passes through.
+// It approaches the collision point by extending the current triangle into a tetrahedron in the direction of the origin.
+// The next triangle is selected from the 3 new faces of the tetrahedron, whichever one intersects the ray.
+// The number of reachable faces triples every iteration, meaning even smooth geometry can be tested in relatively few iterations.
 export default function gjkRaycast3(support: SupportFunction, heading: Vector3, debug?: any) {
     const maxIterations = 10
-    let simplex = gjkRaycastInitialTriangle(support, heading, debug)
+
+    // Returns an arbitrary triangle in the input shape which the ray passes through.
+    let simplex = silhouetteGjk(support, heading, debug)
     if (!simplex)
         return null
+
     let i = 0
     while (true) {
         let [a, b, c] = simplex as Vector3[]
